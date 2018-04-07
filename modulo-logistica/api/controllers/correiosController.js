4a153cb1cbd98d729d933f7c8a0bb17f1e00c865
murilocruz <murilolcruz@gmail.com>
2018-04-06 21:09:48 -0300

    versao inicial do banco criada

--- modulo-logistica/api/controllers/correiosController.js
+++ modulo-logistica/api/controllers/correiosController.js
@@ -1,6 +1,12 @@
 'use strict';
 
+const sqlite3 = require('sqlite3');
+
 
 exports.rastrear_pedido = function(req, res) {
+      let db = new sqlite3.Database('banco.db');
+      console.log('abri o banco');
+      db.close();
+      console.log('fechei o banco');
       res.send("Hello!");
   };
