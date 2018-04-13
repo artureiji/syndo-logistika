fc9c8adae67ec8cb14924bee8b5c6f7c0f413b32
Guilherme Mendes <gmendes@ciandt.com>
2018-04-13 18:40:13 -0300

    Implementação inicial do calcularfrete

--- modulo-logistica/api/controllers/frete.js
+++ modulo-logistica/api/controllers/frete.js
@@ -22,12 +22,12 @@ function calcularDistancia(cepOrigem, cepDestino) {
 
 function calcularVolume(formato, comprimento, altura, largura, diametro) {
     let volume = 0;
-    if (formato === 1) {
+    if (formato == 1) {
         volume = comprimento * altura * largura;
-    } else if (formato === 2) {
+    } else if (formato == 2) {
         volume = comprimento * 3.14159 * diametro;
-    } else if (formato === 3) {
-        volume = 1; // Volume não é importante nesse caso
+    } else if (formato == 3) {
+        volume = 0.4; // Volume não é importante nesse caso, só aplica um fator de redução de preço
     } else {
         console.log("Formato inválido: " + formato);
     }
@@ -44,13 +44,9 @@ function precoSedex() {
 }
 
 function determinaPreco(tipo, distancia, quantidade, peso, formato, volume) {
-    let fator = tipo === 1 ? precoPac() : tipo === 2 ? precoSedex() : 0;
+    let fator = tipo == 1 ? precoPac() : tipo == 2 ? precoSedex() : 0;
 
     let preco = fator * distancia * peso * volume * (quantidade * 0.7);
-
-    if (formato === 1) {
-        preco *= 0.4;
-    }
 
     return preco;
 }
