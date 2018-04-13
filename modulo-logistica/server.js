b5e382ddcdf40bb009fe1ad37fdf5ad861c6a067
murilocruz <murilolcruz@gmail.com>
2018-04-13 17:59:53 -0300

    versao inicial do cadastro de pedido

--- modulo-logistica/server.js
+++ modulo-logistica/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+app.use(bodyParser.json()); // for parsing application/json
 var routes = require('./api/routes/approutes'); //importing route
 routes(app); //register the route
 
