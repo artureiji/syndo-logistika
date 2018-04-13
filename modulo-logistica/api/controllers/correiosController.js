8b78c480685dd8ef8c3394e2020ca3212c5afc1e
Murilo Cruz <murilolcruz@gmail.com>
2018-04-12 21:39:58 -0300

    Versao em andamento da consulta ao banco

--- modulo-logistica/api/controllers/correiosController.js
+++ modulo-logistica/api/controllers/correiosController.js
@@ -2,11 +2,24 @@
 
 const sqlite3 = require('sqlite3');
 
-
-exports.rastrear_pedido = function(req, res) {
-      let db = new sqlite3.Database('banco.db');
-      console.log('abri o banco');
+const com_banco = function(callback) {
+      let db = new sqlite3.Database('./banco.db');
+      console.log('Conectado ao banco');
+      let ret = callback(db);
       db.close();
-      console.log('fechei o banco');
-      res.send("Hello!");
+      console.log('Banco fechado');
+}
+exports.rastrear_pedido = function(req, res) {
+      
+      com_banco(banco => {
+           
+          res.send("Hello!");
+      });
+      
   };
+
+exports.rastrear_entrega = function(req, res) {
+    com_banco(banco => {
+        banco.get("SELECT * FROM Rastreio where codigo = ? ORDER BY codigo;", [req.params.codRastreio], (err, row) => res.send(row));
+    });
+};
