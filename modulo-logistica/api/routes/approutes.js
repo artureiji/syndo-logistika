219296f03d6f97127cfd74c332da90ff86cdd27a
Artur Eiji <artur.eiji@gmail.com>
2018-04-12 23:52:26 -0300

    Prepara rotas

--- modulo-logistica/api/routes/approutes.js
+++ modulo-logistica/api/routes/approutes.js
@@ -1,7 +1,19 @@
 'use strict';
 module.exports = function(app) {
-  const controller = require('../controllers/correiosController');
+  const entrega = require('../controllers/entregaController');
+  const pedido = require('../controllers/pedidoController');
+  const pacote = require('../controllers/pacoteController');
+  const frete = require('../controllers/freteController');
+
+  app.route('/rastrearentrega/:codigoRastreio')
+    .get(controller.rastrear_entrega);
 
   app.route('/rastrearpedido/:idPedido')
     .get(controller.rastrear_pedido);
+
+  app.route('/cadastrarpacote')
+    .post(controller.cadastrar_pacote);
+
+  app.route('/calculafrete')
+    .get(controller.calcular_frete);
 }
