1c7dc405ab80bc43ab32779311f18907cc52b10d
murilocruz <murilolcruz@gmail.com>
2018-04-13 18:13:36 -0300

    v0.1 do endpoint de calcular frete

--- modulo-logistica/api/controllers/frete.js
+++ modulo-logistica/api/controllers/frete.js
@@ -3,7 +3,7 @@
 const database = require('../services/database');
 
 exports.calcular = function(req, res) {
-    let valor = 0;
+    let valor = 5.75;
     let {tipo, ceporigem, cepdestino, quantidade, peso, formato, comprimento, altura, largura, diametro} = req.query;
     //valor = ;
     res.send(valor.toString());
