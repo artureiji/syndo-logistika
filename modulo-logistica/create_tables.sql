fdc9b99e924766706f395ae1eb1747b2601b7978
Gustavo <gustavo.cesar.nunes@gmail.com>
2018-04-14 15:06:01 -0300

    tabela distancia entre os estados

--- modulo-logistica/create_tables.sql
+++ modulo-logistica/create_tables.sql
@@ -1,2 +1,3 @@
 CREATE TABLE Pedido (id_pedido VARCHAR(255), codigo_rastreio VARCHAR(36), PRIMARY KEY(id_pedido, codigo_rastreio));
 CREATE TABLE Rastreio (id_pedido VARCHAR(255) PRIMARY KEY ASC, codigo_rastreio VARCHAR(36), endereco VARCHAR(50), data DATE, mensagem VARCHAR(100));
+CREATE TABLE Distancia (uf_origem VARCHAR(2), uf_destino VARCHAR(2), distancia int, PRIMARY KEY(uf_origem, uf_destino));
