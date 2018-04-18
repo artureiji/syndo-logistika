4dcd75bb9a759487fc6b01821c893013b4a4cfd0
Murilo Cruz <murilo@localhost.localdomain>
2018-04-17 22:04:11 -0300

    tabelas recriadas para suportar api-key e dados do pedido

--- modulo-logistica/create_tables.sql
+++ modulo-logistica/create_tables.sql
@@ -1,3 +1,13 @@
-CREATE TABLE Pedido (id_pedido VARCHAR(255), codigo_rastreio VARCHAR(36), PRIMARY KEY(id_pedido, codigo_rastreio));
-CREATE TABLE Rastreio (id_pedido VARCHAR(255) PRIMARY KEY ASC, codigo_rastreio VARCHAR(36), endereco VARCHAR(50), data DATE, mensagem VARCHAR(100));
+DROP TABLE IF EXISTS Pedido;
+DROP TABLE IF EXISTS Rastreio;
+DROP TABLE IF EXISTS Distancia;
+DROP TABLE IF EXISTS Entrega;
+DROP TABLE IF EXISTS Historico;
+DROP TABLE IF EXISTS Chaves;
+
+
+
+CREATE TABLE Entrega (codigo_rastreio VARCHAR(36) PRIMARY KEY, id_produto VARCHAR(255), tipo_entrega VARCHAR(20), valor INT, cep_origem VARCHAR(9), cep_destino VARCHAR(9), peso INT, tipo_pacote VARCHAR(10), altura INT, largura INT, comprimento INT, api_key VARCHAR(36));
+CREATE TABLE Historico (id int PRIMARY KEY ASC, codigo_rastreio VARCHAR(36), endereco VARCHAR(50), data DATE, mensagem VARCHAR(100), api_key VARCHAR(36));
 CREATE TABLE Distancia (uf_origem VARCHAR(2), uf_destino VARCHAR(2), distancia int, PRIMARY KEY(uf_origem, uf_destino));
+CREATE TABLE Chaves (api_key VARCHAR(36), owner_name VARCHAR(20))
