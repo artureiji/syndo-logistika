4563ba0139918df2fd519a8c5339029197d98363
murilocruz <murilolcruz@gmail.com>
2018-04-27 17:45:07 -0300

    adicionando suporte a  cors

--- modulo-logistica/server.js
+++ modulo-logistica/server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json()); // for parsing application/json
+app.use(cors());
+
 var routes = require('./api/routes/approutes'); //importing route
 routes(app); //register the route
 
